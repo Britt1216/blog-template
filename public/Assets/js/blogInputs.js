d94bec3fb58b857ad7778b03fc47509c12adbc73
Mark Gire <magire01@gmail.com>
2020-08-05 17:43:13 -0400

    Layout1 and layout2 buttons test

--- public/Assets/js/blogInputs.js
+++ public/Assets/js/blogInputs.js
@@ -1,1 +1,9 @@
 console.log("blogInputs.js TEST");
+
+$("#layout1-btn").on("click", function() {
+    console.log("Layout1 TEST");
+});
+
+$("#layout2-btn").on("click", function() {
+    console.log("Layout2 TEST");
+});
