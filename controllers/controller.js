10a91bc1e7b6d7227b8bdad7e0028c68b4bc7e8e
Mark Gire <magire01@gmail.com>
2020-08-06 15:31:59 -0400

    HTML Post Route

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,26 +1,15 @@
 const express = require("express");
 
-
-
 const router = express.Router();
 
-const { Layout1Post }  = require("../models");
-const { Layout2Post }  = require("../models");
-
 router.get("/", (req, res) => {
+    
     res.render("index");
 })
 
-
 router.get("/post", (req, res) => {
-    Layout2Post.create(req.body)
     res.render("post");
 });
 
-
-router.post("/post/new", (req, res) => {
-    Layout1Post.create()
-    res.render("post");
-});
 
 module.exports = router;
