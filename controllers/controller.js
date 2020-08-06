de300777b755a4f7f55df7cc854d25dd08a6f8df
Mark Gire <magire01@gmail.com>
2020-08-06 15:44:08 -0400

    Pull issues fixed, now up to date

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,13 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-<<<<<<< HEAD
-=======
-const { Layout1Post }  = require("../models");
-
->>>>>>> master
 router.get("/", (req, res) => {
-    
     res.render("index");
 })
 
@@ -16,12 +10,5 @@ router.get("/post", (req, res) => {
     res.render("post");
 });
 
-<<<<<<< HEAD
-=======
-router.post("/post/new", (req, res) => {
-    Layout1Post.create()
-    res.render("post");
-});
->>>>>>> master
 
 module.exports = router;
