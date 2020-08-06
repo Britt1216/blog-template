fc36a8035e7bf0ee8c01485b51234cf3377687ea
Brittny Rosenow <brittnyrosenow@gmail.com>
2020-08-05 18:35:10 -0600

    added layout2

--- public/Assets/js/blogInputs.js
+++ public/Assets/js/blogInputs.js
@@ -7,6 +7,7 @@ $("#layout1-btn").on("click", function() {
 
 $("#layout2-btn").on("click", function() {
     console.log("Layout2 TEST");
+    layout2Input();
 });
 
 const layout1Input = () => {
@@ -32,4 +33,30 @@ const layout1Input = () => {
     inputDiv.append(`</form>`);
     inputDiv.append(`</div>`);
     inputDiv.append(`</div>`);
+};
+
+const layout2Input = () => {
+    const inputDiv = $("#input-div")
+
+    inputDiv.append(`<form>`);
+    inputDiv.append(`<div class="form-group">`);
+    inputDiv.append(`<label>Title</label>`);
+    inputDiv.append(`<input class="form-control" id="title-input">`);
+    inputDiv.append(`<label>Img1</label>`);
+    inputDiv.append(`<label>Header 1</label>`);
+    inputDiv.append(`<input class="form-control" id="header1-input">`);
+    inputDiv.append(`<label>Text 1</label>`);
+    inputDiv.append(`<textarea class="form-control" id="text1-input" rows="3"></textarea>`);
+    inputDiv.append(`<label>Side Header </label>`);
+    inputDiv.append(`<input class="form-control" id="header2-input">`);
+    inputDiv.append(`<label>Side Text</label>`);
+    inputDiv.append(`<textarea class="form-control" id="text2-input" rows="3"></textarea>`);
+    inputDiv.append(`<label>Category</label>`);
+    inputDiv.append(`<input class="form-control" id="category">`);
+    inputDiv.append(`</div>`);
+    inputDiv.append(`</form>`);
+    inputDiv.append(`</div>`);
+    inputDiv.append(`</div>`);
+
+
 }
