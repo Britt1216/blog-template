9290a3ddb211b7ac56195269310346656190995e
Mark Gire <magire01@gmail.com>
2020-08-07 02:21:17 -0400

    get findAll function now displays layout2 table on blog page

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,25 +1,34 @@
 
-
 const express = require("express");
-
 const router = express.Router();
 
-const { Layout2Post } = require("../models");
+
+
+const { Layout2Post } = require("../models/");
 
 router.get("/", (req, res) => {
-    res.render("index");
+    Layout2Post.findAll().then(results => {
+        const context = {
+            post: results.map(content => {
+              return {
+                title: content.title,
+                img1: content.img1,
+                header1: content.header1,
+                text1: content.text1,
+                sideHead: content.sideHead,
+                sideText: content.sideText,
+                category: content.category
+              }
+            })
+          }
+        res.render("index", { post: context.post });
+    })
 })
 
 router.get("/post", (req, res) => {
     res.render("post");
 });
 
-// router.get("/api/posts", (req, res) => {
-//     Post.findAll().then(results => {
-//         res.json(results);
-//         res.end();
-//     })
-// })
 
 router.post("/api/posts", (req, res) => {
     Layout2Post.create(req.body).then(results => {
